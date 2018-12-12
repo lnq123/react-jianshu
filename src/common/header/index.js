8e2a34c6d8092db66018da0971fe4a43f7e0d06a
shadowstream <nanqingl@qq.com>
2018-12-12 15:22:54 +0800

    搜索弹窗

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -22,21 +22,41 @@ import {
 import MyIcon from "../../icon";
 
 class Header extends Component {
-  
   getListArea() {
-    const { focused,list} =this.props
-    if (focused) {
+    const {
+      focused,
+      totalPage,
+      list,
+      mouseIn,
+      page,
+      handleMouseEnter,
+      handleChangePage,
+      handleMouseLeave
+    } = this.props;
+    const newList = list.toJS();
+    const pageList = [];
+
+    if (newList.length) {
+      for (let i = (page - 1) * 10; i < page * 10; i++) {
+        pageList.push(
+          <SearchInfoItem key={newList[i]}>{newList[i]}</SearchInfoItem>
+        );
+      }
+    }
+
+    if (focused || mouseIn) {
       return (
         <SearchInfo>
-          <SearchInfoTitile>
+          <SearchInfoTitile
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+          >
             热门搜索
-            <SearchInfoSwitch>换一批</SearchInfoSwitch>
+            <SearchInfoSwitch onClick={() => handleChangePage(page, totalPage)}>
+              换一批
+            </SearchInfoSwitch>
           </SearchInfoTitile>
-          <SearchInfoList>
-            {list.map(item => {
-              return <SearchInfoItem key={item}>{item}</SearchInfoItem>;
-            })}
-          </SearchInfoList>
+          <SearchInfoList>{pageList}</SearchInfoList>
         </SearchInfo>
       );
     } else {
@@ -45,7 +65,7 @@ class Header extends Component {
   }
 
   render() {
-    const { focused,handleInputFocus,handleInputBlur} =this.props
+    const { focused, handleInputFocus, handleInputBlur } = this.props;
     return (
       <HeaderWrapper>
         <Logo />
@@ -58,7 +78,7 @@ class Header extends Component {
           </NavItem>
           <SearchWrapper>
             <CSSTransition
-              in={focused}
+              in={this.props.focused}
               timeout={200}
               classNames="slide"
             >
@@ -90,11 +110,13 @@ const mapStateToProps = state => {
   return {
     // focused:state.get('header').get('focused')
     focused: state.getIn(["header", "focused"]),
-    list: state.getIn(["header", "list"])
+    list: state.getIn(["header", "list"]),
+    page: state.getIn(["header", "page"]),
+    mouseIn: state.getIn(["header", "mouseIn"]),
+    totalPage: state.getIn(["header", "totalPage"])
   };
 };
 const mapDispathToProps = dispatch => {
-  
   return {
     handleInputFocus() {
       dispatch(actionCreators.getList());
@@ -102,6 +124,19 @@ const mapDispathToProps = dispatch => {
     },
     handleInputBlur() {
       dispatch(actionCreators.searchBlur());
+    },
+    handleMouseEnter() {
+      dispatch(actionCreators.mouseEnter());
+    },
+    handleMouseLeave() {
+      dispatch(actionCreators.mouseLeave());
+    },
+    handleChangePage(page, totalPage) {
+      if (page < totalPage) {
+        dispatch(actionCreators.changePage(page + 1));
+      } else {
+        dispatch(actionCreators.changePage(1));
+      }
     }
   };
 };
--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -4,7 +4,10 @@ import { fromJS } from "immutable";
 
 const defaultState = fromJS({
   focused: false,
-  list: []
+  list: [],
+  page:1,
+  totalPage:1,
+  mouseIn:false,
 });
 
 export default (state = defaultState, action) => {
@@ -14,7 +17,13 @@ export default (state = defaultState, action) => {
     case constants.SEARCH_BLUR:
       return state.set("focused", false);
     case constants.CHANGE_LIST:
-      return state.set("focused", action.data);
+      return state.set("list", action.data).set('totalPage',action.totalPage);
+      case constants.MOUSE_ENTER:
+      return state.set("mouseIn", true)
+      case constants.MOUSE_LEAVE:
+      return state.set("mouseIn", false)
+      case constants.CHANGE_PAGE:
+      return state.set("page", action.page)
     default:
       return state;
   }
