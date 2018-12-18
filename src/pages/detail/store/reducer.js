1b6363ca758b37bc466ec98ef245c0bd28f3bb47
shadowstream <nanqingl@qq.com>
2018-12-18 16:13:30 +0800

    detail reducer

--- src/pages/detail/store/reducer.js
+++ src/pages/detail/store/reducer.js
@@ -2,19 +2,17 @@ import { fromJS } from "immutable";
 import * as constants from "./constants";
 
 const defaultState = fromJS({
-  title:
-    "",
-  content:
-    ""
+  title: "",
+  content: ""
 });
 
 export default (state = defaultState, action) => {
   switch (action.type) {
     case constants.CHANGE_DETAIL:
       return state.merge({
-        title:action.title,
-        content:action.content,
-      })
+        title: action.title,
+        content: action.content
+      });
     default:
       return state;
   }
